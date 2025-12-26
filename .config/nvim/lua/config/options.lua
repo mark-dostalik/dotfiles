b74b04d8a3cd13b47d8e5faadf38eea7b4ee8804
Mark Dostalík <mark.dostalik@gmail.com>
2025-12-26 12:25:40 +0100

    Switch from vim to neovim

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,3 @@
+vim.g.lazyvim_python_lsp = "basedpyright"
+vim.g.autoformat = false
+vim.diagnostic.enable(false)
---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      transparent_background = false,
+      term_colors = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
