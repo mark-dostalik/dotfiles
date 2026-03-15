bdd88726da5c51836b36871c0262096e2d063366
Mark Dostalík <mark.dostalik@gmail.com>
2026-03-15 07:10:07 +0100

    Swap basedpyright with pyright

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,16 +1,16 @@
 vim.opt.clipboard = "unnamedplus"
 vim.g.clipboard = {
-  name = 'OSC 52',
+  name = "OSC 52",
   copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
   },
   paste = {
-    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
   },
 }
 
-vim.g.lazyvim_python_lsp = "basedpyright"
-vim.g.autoformat = false
-vim.diagnostic.enable(false)
+vim.g.lazyvim_python_lsp = "pyright"
+vim.g.autoformat = true
+-- vim.diagnostic.enable(false)
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,23 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    inlay_hints = { enabled = false },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                typeCheckingMode = "basic",
+                diagnosticMode = "openFilesOnly",
+                diagnosticSeverityOverrides = {
+                  reportUnusedVariable = "warning",
+                  reportUnusedImport = "warning",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
   },
 }
